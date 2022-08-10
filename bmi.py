be0b09d193451c4171928aadd9583097d6a607ae
Praveen Gudipalli <praveengudipalli3@gmail.com>
2022-08-10 19:10:49 +0530

    Body mass index

---
+++ bmi.py
@@ -0,0 +1,10 @@
+weight = float(input("Enter your weight kg: "))
+height = float(input("Enter your height m: "))
+ 
+bmi = weight/height**2
+print(bmi)
+
+weight = float(input("Enter your weight pounds"))
+height = float(input("Enter your height inches"))
+
+bmi = weight
