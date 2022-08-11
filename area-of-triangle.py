c29a018cddf1d9f0bb110c575ae863860ebe5410
Praveen Gudipalli <praveengudipalli3@gmail.com>
2022-08-11 22:07:31 +0530

    area of triangle

---
+++ area-of-triangle.py
@@ -0,0 +1,14 @@
+"""The area of a triangle can be computed using the following formula.where b is the length
+of the base of the triangle, and h is its height:
+
+area = b * h / 2
+
+Write a program that allows the user to enter values for b and h. The program should then
+compute and display the area of a triangle with base length b and height h."""
+
+b = int(input("Enter the base value: "))
+h = int(input("Enter the height value: "))
+
+area = b*h/2
+
+print("Area of Triangle", area)
