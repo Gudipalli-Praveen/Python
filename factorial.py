1064d3fa52d4bfa0a45979eb22405ad66475d042
Praveen Gudipalli <praveengudipalli3@gmail.com>
2022-09-01 20:04:07 +0530

    factorial of a number

---
+++ factorial.py
@@ -0,0 +1,21 @@
+# For Loop
+
+from re import I
+
+'''
+n=int(input("Enter any number: "))
+j=1
+for i in range(1,n+1):
+    j=j*i
+print(j)
+'''
+
+# While Loop
+
+n=int(input("Enter any number: "))
+i=1
+j=1
+while i<=n:
+    j=j*i
+    i=i+1
+print(j)
