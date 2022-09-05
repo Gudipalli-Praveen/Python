48d39c7b4079079ba9354438876c2cca8aed9aac
Praveen Gudipalli <praveengudipalli3@gmail.com>
2022-09-05 17:03:06 +0530

    star pattern

---
+++ star-pattern.py
@@ -0,0 +1,82 @@
+# increasing triangle star
+'''n = 6
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()'''
+
+# decreasing triangle star
+'''n = 5
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()'''
+
+# right side increasing triangle
+'''n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()'''
+
+#right side decreasing triangle
+'''n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()'''
+
+# hill pattern
+'''n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()'''
+
+# reverse hill pattern
+'''n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()'''
+
+# diamond pattern
+'''n = 5
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()'''
+
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print(j+1,end=" ")
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
